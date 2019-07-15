3833ba4bbaff8a14c1df0501908982399ba6a139
momokASC6 <mikuniku3@gmail.com>
2019-07-15 14:56:26 -0400

    Links p5.js and sketch.js. Creates canvas. Adds mouse dragging circles

---
+++ sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+    createCanvas(500,500); // Set canvas to 500x500
+}
+
+function mouseDragged() {
+    // Create a circle with origin of the mouse's X and Y coordinates
+    //circle(mouseX, mouseY, 100);
+}
