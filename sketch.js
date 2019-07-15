5559a32a829591f547d0cc305cb2f0e806b0f170
momokASC6 <mikuniku3@gmail.com>
2019-07-15 15:09:25 -0400

    Add 3 boxes at the top

--- sketch.js
+++ sketch.js
@@ -1,8 +1,34 @@
+var fillColor = "#fae";
+var boxWidth = 200;
+var boxHeight = 100;
+
+var blueBoxX = 0;
+var redBoxX = 250;
+var greenBoxX = 500;
+
 function setup() {
-    createCanvas(500,500); // Set canvas to 500x500
+    createCanvas(1000,1000); // Set canvas to 500x500
+
+    fill('blue');
+    rect(blueBoxX, 0, boxWidth, boxHeight);
+
+    fill('red');
+    rect(redBoxX, 0, boxWidth, boxHeight);
+
+    fill('green');
+    rect(greenBoxX, 0, boxWidth, boxHeight);
 }
 
 function mouseDragged() {
+    fill(fillColor);
     // Create a circle with origin of the mouse's X and Y coordinates
     //circle(mouseX, mouseY, 100);
+
+    // Create a circle with mouse origins but different size each time
+    circle(mouseX, mouseY, random(50,300));
 }
+
+function mouseClicked() {
+
+}
+
